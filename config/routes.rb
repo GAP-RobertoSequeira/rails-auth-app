b744b67f8abef325d83c837637daf2df8490774a
Roberto Sequeira <rsequeira@growthaccelerationpartners.com>
2017-06-14 17:21:47 -0600

    Add devise gem to manage user registration and session management

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for(
+    :users,
+    path: '',
+    path_names: {
+      sign_in: 'login',
+      sign_out: 'logout',
+      sign_up: 'register',
+      edit: 'profile'
+    }
+  )
+
+  get 'dashboard', to: 'dashboard#index'
+
+  root 'home#index'
+
 end
